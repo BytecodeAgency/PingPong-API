bff665e9219934d0161d8dbb7195d96f47b74e6f
Luciano Nooijen <luciano@bytecode.nl>
2019-03-02 16:58:06 +0100

    Added example server

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,13 @@
-console.log('working');
+import express from 'express';
+import routes from '../routes';
+import middleware from '../middleware';
+
+const app = express();
+middleware(app);
+app.use('/', routes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    // tslint:disable-next-line
+    console.log(`Server listening on port ${PORT}`);
+});
