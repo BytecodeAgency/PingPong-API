96774b68ba904fe72381806e3fa2837804c64a8d
Luciano Nooijen <luciano@bytecode.nl>
2019-03-21 17:29:44 +0100

    Remove comment no longer needed

--- tests/config.ts
+++ tests/config.ts
@@ -3,7 +3,6 @@ import knex from '../helpers/db';
 const rollbackMigrateAndFill = async () =>
     knex.migrate
         .rollback()
-        // .then(() => knex.migrate.latest().then(() => knex.seed.run()));
         .then(() => knex.migrate.latest());
 
 const prepareDatabase = (done: any): void => {
