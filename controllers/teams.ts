658eb6f90f56091b8c8a77a81e016df73216dcbc
Jeroen van Steijn <jjvansteijn@student.tudelft.nl>
2019-03-23 22:30:09 +0100

    register team implementation

--- controllers/teams.ts
+++ controllers/teams.ts
@@ -1,8 +1,17 @@
 import { Request, Response } from 'express';
+import Team from '../models/team';
+
+const registerTeam = async(req: Request, res: Response): Promise<void> => {
+    const teamData = {
+        name: req.body.name,
+    };
+    const team = Team.addNewTeam(teamData);
+}
 
 // TODO: Research middleware for auth
 export default class TeamController {
     public static create = (req: Request, res: Response): void => {
+        registerTeam(req, res);
         res.sendStatus(200);
     }
     public static getAll = (req: Request, res: Response): void => {
