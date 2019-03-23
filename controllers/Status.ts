d418239e702fff28ff0f9e54cf9520a6aa59f143
Luciano Nooijen <luciano@bytecode.nl>
2019-03-23 17:11:37 +0100

    delete status (temp)

--- controllers/Status.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-
-export default class StatusController {
-    public static ok = (req: Request, res: Response): void => {
-        res.sendStatus(200);
-    }
-}
