7e24782d9c299442bc542f2d32d92b3be411fec8
Luciano Nooijen <luciano@bytecode.nl>
2019-04-13 18:43:47 +0200

    Added TODOs
    Signed-off-by: Luciano Nooijen <luciano@bytecode.nl>

--- routes/routes.ts
+++ routes/routes.ts
@@ -8,7 +8,7 @@ router.get('/status', Status.ok);
 router.post('/player/authenticate', Player.auth);
 router.post('/player/register', Player.register);
 router.post('/team/create', Team.create);
-router.post('/team/get-all-data', Team.getAll);
-router.post('/game/create', Game.createGame);
+router.post('/team/get-all-data', Team.getAll); // TODO
+router.post('/game/create', Game.createGame); // TODO
 
 export default router;
